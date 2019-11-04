643480402f590ef25a9c083a044d819b507848f7
neerajkj86 <JainNe@INGUEUCLP000666.camkmg-bangalore.com>
2019-11-05 02:10:24 +0530

    merge check

--- goibobo/src/test/java/handledropdown/Assignment1.java
+++ goibobo/src/test/java/handledropdown/Assignment1.java
@@ -29,7 +29,7 @@ public class Assignment1
 	driver = new FirefoxDriver();
 	
 	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 	
 	
 	driver.get("https://www.google.com/");
